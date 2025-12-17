13d369a1d3ea3e0e8a305432db7bbb28303071a7
Andrew Sander <167649347+AndrewSander@users.noreply.github.com>
2025-12-17 20:01:09 +0000

    save 01

---
+++ atividade-cep/app/page.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import React from 'react';
+import './globals.css';
+
+export default function Home(){
+  const [cep, setCep] = React.useState("");
+  const [rua, setRua] = React.useState("");
+  const [numero, setNumero] = React.useState("");
+  const [bairro, setBairro] = React.useState("");
+  const [estado, setEstado] = React.useState("");
+  const [cidade, setCidade] = React.useState("");
+
+  async function getDados(){
+    try{
+
+      const address = await ((await fetch(`https://viacep.com.br/ws/${cep}/json/`)).json())
+      return address;
+
+    }
+    catch (erro){
+      console.log(erro)
+    }
+  }
+
+  React.useEffect(() => {
+    if (cep.length === 8){
+      async function pegarCpf(){
+        const address = await (getDados());
+        if (address){
+          console.log(address)
+          if(!address.erro){
+            setRua(address.logradouro);
+            setBairro(address.bairro);
+            setEstado(address.uf);
+            setCidade(address.localidade);
+          }
+          else{
+            console.log("erro");
+          }
+        }
+      }
+      pegarCpf();
+    }
+  },[cep])
+
+  getDados();
+
+  return (
+    <>
+      <form>
+        <input type="text" value={cep} placeholder='CEP' onChange={(a) => setCep(a.target.value)}/>
+        <input type="text" value={rua} placeholder='RUA' onChange={(a) => setCep(a.target.value)}/>
+        <input type="text" value={numero} placeholder='NUMERO' onChange={(a) => setCep(a.target.value)}/>
+        <input type="text" value={bairro} placeholder='BAIRRO' onChange={(a) => setCep(a.target.value)}/>
+        <input type="text" value={estado} placeholder='ESTADO' onChange={(a) => setCep(a.target.value)}/>
+        <input type="text" value={cidade} placeholder='CIDADE' onChange={(a) => setCep(a.target.value)}/>
+      </form>
+    </>
+  )
+
+}
