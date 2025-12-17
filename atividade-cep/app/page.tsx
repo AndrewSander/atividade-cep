cc6eab745da02b9252ee6fdf6d0ac7134b49560d
Andrew Sander <167649347+AndrewSander@users.noreply.github.com>
2025-12-17 23:35:22 +0000

    save 03

--- atividade-cep/app/page.tsx
+++ atividade-cep/app/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import './globals.css';
 
 export default function Home(){
+  const [notFound, setNotFound] = React.useState(false);
   const [erro,setErro] = React.useState(false);
   const [cep, setCep] = React.useState("");
   const [rua, setRua] = React.useState("");
@@ -11,6 +12,7 @@ export default function Home(){
   const [bairro, setBairro] = React.useState("");
   const [estado, setEstado] = React.useState("");
   const [cidade, setCidade] = React.useState("");
+
 
   async function getDados(){
     try{
@@ -30,11 +32,16 @@ export default function Home(){
         if (address){
           console.log(address)
           if(!address.erro){
+            setNotFound(false);
             setErro(false);
             setRua(address.logradouro);
             setBairro(address.bairro);
             setEstado(address.uf);
             setCidade(address.localidade);
+          }
+          else{
+            setNotFound(true);
+            console.log("QUE RAIVAA");
           }
         }
       }
@@ -42,18 +49,39 @@ export default function Home(){
     }
   },[cep])
 
-  getDados();
+  function changeError(){
+    if (cep.length != 8 && cep.length > 0){
+      setErro(true);
+      setRua("");
+      setNumero("");
+      setBairro("");
+      setEstado("");
+      setCidade("");
+    }
+    else if(notFound){
+      setErro(true);
+      setRua("");
+      setNumero("");
+      setBairro("");
+      setEstado("");
+      setCidade("");
+    }
+    else{
+      setErro(false);
+    }
+  }
 
   return (
     <>
+      <h1>ADDRESS</h1>
       <form>
-        <input type="text" value={cep} placeholder='CEP' onChange={(a) => setCep(a.target.value)}/>
-        {(erro && <p>CEP INVÁLIDO</p>)}
-        <input type="text" value={rua} placeholder='RUA' onChange={(a) => setCep(a.target.value)}/>
-        <input type="text" value={numero} placeholder='NUMERO' onChange={(a) => setCep(a.target.value)}/>
-        <input type="text" value={bairro} placeholder='BAIRRO' onChange={(a) => setCep(a.target.value)}/>
-        <input type="text" value={estado} placeholder='ESTADO' onChange={(a) => setCep(a.target.value)}/>
-        <input type="text" value={cidade} placeholder='CIDADE' onChange={(a) => setCep(a.target.value)}/>
+        <input type="text" className={erro ? 'cep-error' : 'cep' } value={cep} placeholder='CEP' onBlur={(a) => changeError()} onChange={(a) => setCep(a.target.value)}/>
+        {(erro && <p>O CEP INFORMADO É INVÁLIDO</p>)}
+        <input type="text" value={rua} placeholder='RUA' onChange={(a) => setRua(a.target.value)}/>
+        <input type="text" value={numero} placeholder='NUMERO' onChange={(a) => setNumero(a.target.value)}/>
+        <input type="text" value={bairro} placeholder='BAIRRO' onChange={(a) => setBairro(a.target.value)}/>
+        <input type="text" value={estado} placeholder='ESTADO' onChange={(a) => setEstado(a.target.value)}/>
+        <input type="text" value={cidade} placeholder='CIDADE' onChange={(a) => setCidade(a.target.value)}/>
       </form>
     </>
   )
