a0a5b1ef820da55e7ccc1d5a9a672ebc012a7d05
Andrew Sander <167649347+AndrewSander@users.noreply.github.com>
2025-12-17 20:39:17 +0000

    save 02

--- atividade-cep/app/page.tsx
+++ atividade-cep/app/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import './globals.css';
 
 export default function Home(){
+  const [erro,setErro] = React.useState(false);
   const [cep, setCep] = React.useState("");
   const [rua, setRua] = React.useState("");
   const [numero, setNumero] = React.useState("");
@@ -13,13 +14,12 @@ export default function Home(){
 
   async function getDados(){
     try{
-
       const address = await ((await fetch(`https://viacep.com.br/ws/${cep}/json/`)).json())
       return address;
 
     }
     catch (erro){
-      console.log(erro)
+      console.log("entrei aqui")
     }
   }
 
@@ -30,13 +30,11 @@ export default function Home(){
         if (address){
           console.log(address)
           if(!address.erro){
+            setErro(false);
             setRua(address.logradouro);
             setBairro(address.bairro);
             setEstado(address.uf);
             setCidade(address.localidade);
-          }
-          else{
-            console.log("erro");
           }
         }
       }
@@ -50,6 +48,7 @@ export default function Home(){
     <>
       <form>
         <input type="text" value={cep} placeholder='CEP' onChange={(a) => setCep(a.target.value)}/>
+        {(erro && <p>CEP INVÁLIDO</p>)}
         <input type="text" value={rua} placeholder='RUA' onChange={(a) => setCep(a.target.value)}/>
         <input type="text" value={numero} placeholder='NUMERO' onChange={(a) => setCep(a.target.value)}/>
         <input type="text" value={bairro} placeholder='BAIRRO' onChange={(a) => setCep(a.target.value)}/>
